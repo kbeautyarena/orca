paidaxingyo666/Manta

Make the structured turn lifecycle row durable so completed durations survive (#19695)
* Make the structured turn lifecycle row durable so completed durations survive

A structured-chat turn used to end by tombstoning its running lifecycle item,
which threw away the only durable record of when the turn ended. Completed
"Worked for" labels therefore depended on the renderer having observed the
turn finish, and vanished on reopen.

The lifecycle item is now revised in place, never tombstoned:
- running, with startedAt, at the provider's turn start
- completed or interrupted, with completedAt, at the provider's terminal frame,
  a user stop, or a child exit the host observed
- unverifiable, with no end, when a cold acquire finds a running row from a
  generation whose exit nobody observed

Both timestamps are the execution host's clock at receipt, captured before the
deferred sink, so the completed value is identical on every client and needs
no client clock. Codex history restore uses the provider's own second-granular
endpoints for turns that predate this change. Desktop and mobile read settled
durations off the journal through one shared selector, and anchor the live
counter on the host start with the client's local receipt so a skewed client
clock never leaks into the label. Locally observed durations remain the
fallback for hosts that still tombstone.

Timestamps live inside the existing turnLifecycle field, which old clients
strip, and every working-state consumer keys on state === 'running', so no
capability negotiation is needed.

* native-chat: avoid stale working status on settled turns

* test: align settled turn status expectations

* Name settled lifecycle rows by their terminal state

An interrupted or unverifiable turn must not read as completed for any
consumer that renders status text raw. One shared helper builds the text for
both providers from the lifecycle state.

* test: deduplicate turn lifecycle suites

Each behavior keeps one test; duplicated h

src/main/native-chat/agent-session-journal/journal-row-schema-version.test.ts
@@ -1,0 +1,66 @@
+import { mkdtemp, rm } from 'node:fs/promises'
+import { tmpdir } from 'node:os'
+import { join } from 'node:path'
+import { afterEach, beforeEach, describe, expect, it } from 'vitest'
+import { agentJournalTurnBody } from '../../../shared/agent-session-turn-record'
+import { openJournalDatabase } from './journal-database'
+import { journalDatabaseFile } from './journal-paths'
+import { createTrackedJournalOpener } from './journal-store-test-open'
+
+// Which rows an older host can still read: only rows that carry a turn item
+// are stamped with the version it does not know, and the epoch row never is.
+// Read raw: the reader upcasts every row to the current version, so only the
+// stored row_json says what an older build would see.
+describe('journal row schema versions', () => {
+  let root = ''
+  const opener = createTrackedJournalOpener()
+
+  beforeEach(async () => {
+    root = await mkdtemp(join(tmpdir(), 'orca-journal-row-version-'))
+  })
+  afterEach(async () => {
+    await opener.closeAll()
+    await rm(root, { recursive: true, force: true })
+  })
+
+  it('stamps v3 only on rows that carry a turn item', async () => {
+    const journal = await opener.open({
+      identity: {
+        sessionId: 'session-1',
+        workspaceId: 'workspace-1',
+        hostId: 'local',
+        agent: 'codex',
+        providerHandle: { kind: 'codex', threadId: 'thread-1' }
+      },
+      now: () => 1_000,
+      journalDir: join(root, 'session-1')
+    })
+    const identity = { provider: 'manta' as const, clientMessageId: 'm1' }
+    await journal.appendItem(
+      identity,
+      { kind: 'message', role: 'user', blocks: [{ type: 'text', text: 'hi' }] },
+      { fence: 1 }
+    )
+    await journal.appendItem(
+      { provider: 'legacy', agent: 'codex', sessionId: 'session-1', recordId: 'turn-lifecycle:t1' },
+      agentJournalTurnBody({ turnId: 't1', state: 'running', startedAt: 1_000 }),
+      { fence: 1 }
+    )
+    await journal.close()
+    const opened = openJournalDatabase(journalDatabaseFile(join(root, 'session-1')))
+    try {
+      const stored = opened.db
+        .prepare('SELECT row_json FROM journal_rows ORDER BY seq')
+        .all()
+        .map((row) => JSON.parse(String((row as { row_json: string }).row_json)))
+        .map((row: { kind: string; v: number }) => [row.kind, row.v])
+      expect(stored).toEqual([
+        ['epoch', 2],
+        ['item', 2],
+        ['item', 3]
+      ])
+    } finally {
+      opened.db.close()
+    }
+  })
+})

src/main/native-chat/agent-session-wire/structured-agent-session-subscribers.test.ts
@@ -68,8 +68,54 @@ describe('AgentSessionSubscribers', () => {
           submissions: []
         },
         fence: 7,
+        hostNow: expect.any(Number),
         activity: null
       }
+    ])
+  })
+
+  it('stamps the host clock once per published frame', async () => {
+    const journal = await journals.open({
+      identity: {
+        sessionId: SESSION,
+        workspaceId: 'workspace-1',
+        hostId: 'local',
+        agent: 'codex',
+        providerHandle: { kind: 'codex', threadId: 'thread-1' }
+      },
+      journalDir: join(root, 'clock-journal')
+    })
+    let now = 1_000
+    const events: AgentSessionSubscribeEvent[] = []
+    const subscribers = new AgentSessionSubscribers({ now: () => (now += 1) })
+    const emit = (event: AgentSessionSubscribeEvent): void => {
+      events.push(event)
+    }
+    subscribers.open({ id: 'one', sessionId: SESSION, journal, fence: 1, emit })
+    subscribers.open({ id: 'two', sessionId: SESSION, journal, fence: 1, emit })
+    await journal.appendItem(
+      { provider: 'manta', clientMessageId: 'clocked' },
+      { kind: 'status', text: 'Clocked' },
+      { fence: 1 }
+    )
+    subscribers.publish(SESSION, journal)
+    subscribers.handoff(SESSION, 1, { owner: 'native' } as AgentSessionHandoffStatus)
+    subscribers.reset(SESSION, journal, 'epoch_changed', 1)
+
+    expect(events.map((event) => ('hostNow' in event ? event.hostNow : null))).toEqual([
+      1_001, 1_002,
+      // Both subscribers of one publication read the same clock sample.
+      1_003, 1_003, 1_004, 1_004, 1_005, 1_005
+    ])
+    expect(events.map((event) => event.type)).toEqual([
+      'snapshot',
+      'snapshot',
+      'batch',
+      'batch',
+      'batch',
+      'batch',
+      'reset',
+      'reset'
     ])
   })
 
@@ -101,6 +147,7 @@ describe('AgentSessionSubscribers', () => {
       type: 'batch',
       sessionId: SESSION,
       fence: 7,
+      hostNow: expect.any(Number),
       commands,
       batch: { cursor: journal.cursor(), items: [], removedItemIds: [], submissions: [] }
     })
@@ -245,6 +292,7 @@ describe('AgentSessionSubscribers', () => {
         submissions: []
       },
       fence: 2,
+      hostNow: expect.any(Number),
       handoff
     })
   })
@@ -284,6 +332,7 @@ describe('AgentSessionSubscribers', () => {
       sessionId: SESSION,
       batch: { cursor, items: [], removedItemIds: [], submissions: [] },
       fence: 2,
+      hostNow: expect.any(Number),
       backgroundTasks
     })
 
@@ -330,6 +379,7 @@ describe('AgentSessionSubscribers', () => {
       sessionId: SESSION,
       batch: { cursor, items: [], removedItemIds: [], submissions: [] },
       fence: 1,
+      hostNow: expect.any(Number),
       activity: { turnId: 'turn-1', text: 'Inspecting the session wire' }
     })
 

src/shared/native-chat-unverifiable-turn-status.test.ts
@@ -1,0 +1,134 @@
+import { describe, expect, it } from 'vitest'
+import type { AgentJournalRenderItem, AgentJournalTurnItem } from './agent-session-journal-types'
+import { reduceNativeChatTurnTiming, selectNativeChatTurnStatuses } from './native-chat-turn-status'
+import { selectStructuredAgentSettledTurns } from './structured-agent-session-turn-timing'
+
+function userItem(itemId: string): AgentJournalRenderItem {
+  return {
+    itemId,
+    revision: 0,
+    sequence: 0,
+    observedAt: 1_000,
+    body: { kind: 'message', role: 'user', blocks: [{ type: 'text', text: 'hello' }] }
+  }
+}
+
+describe('authoritative unknown turn duration at the shared status consumer', () => {
+  it.each([1_000, undefined])(
+    'does not convert a running turn to local Worked for after unverifiable recovery (start %s)',
+    (startedAt) => {
+      const user: AgentJournalRenderItem = {
+        itemId: 'manta:u1',
+        revision: 0,
+        sequence: 1,
+        observedAt: 1_000,
+        body: { kind: 'message', role: 'user', blocks: [{ type: 'text', text: 'hello' }] }
+      }
+      const recoveredTurn: AgentJournalRenderItem = {
+        itemId: 'legacy:codex:s:turn-lifecycle%3At1',
+        revision: 2,
+        sequence: 2,
+        observedAt: 60_000,
+        body: {
+          kind: 'turn',
+          turnId: 't1',
+          state: 'unverifiable',
+          ...(startedAt === undefined ? {} : { startedAt, userItemId: user.itemId })
+        }
+      }
+      const keys = new Set([user.itemId])
+      const running = reduceNativeChatTurnTiming(
+        {},
+        {
+          activeTurnKey: user.itemId,
+          validTurnKeys: keys,
+          isWorking: true,
+          workingStartedAt: 1_000,
+          now: 1_000
+        }
+      )
+      const locallyStopped = reduceNativeChatTurnTiming(running, {
+        activeTurnKey: user.itemId,
+        validTurnKeys: keys,
+        isWorking: false,
+        workingStartedAt: null,
+        now: 60_000
+      })
+      const options = {
+        activeTurnKey: user.itemId,
+        isWorking: false,
+        hasCurrentTurnResponse: true,
+        settledByTurn: selectStructuredAgentSettledTurns([user, recoveredTurn])
+      }
+
+      const mounted = selectNativeChatTurnStatuses(locallyStopped, options)
+      const reloaded = selectNativeChatTurnStatuses({}, options)
+      expect(mounted).toEqual(reloaded)
+      expect(mounted.active).toBeNull()
+      expect(mounted.completedByTurn).toEqual({})
+    }
+  )
+
+  it.each(['running', 'completed', 'interrupted'] as const)(
+    'suppresses local completion for a host-recorded %s turn without an endpoint',
+    (state) => {
+      const item: AgentJournalRenderItem = {
+        itemId: 'lifecycle-1',
+        revision: 1,
+        sequence: 1,
+        observedAt: 1_000,
+        body: { kind: 'turn', turnId: 't1', state, startedAt: 1_000, userItemId: 'turn-1' }
+      }
+      const statuses = selectNativeChatTurnStatuses(
+        {
+          'turn-1': { startedAt: 1_000, workedSeconds: 59 },
+          next: { startedAt: 60_000, workedSeconds: null }
+        },
+        {
+          activeTurnKey: 'next',
+          isWorking: true,
+          hasCurrentTurnResponse: true,
+          settledByTurn: selectStructuredAgentSettledTurns([userItem('turn-1'), item])
+        }
+      )
+      expect(statuses.completedByTurn).toEqual({})
+      expect(statuses.active).toEqual({ startedAt: 60_000, workedSeconds: null, thinking: false })
+    }
+  )
+
+  it('uses provider duration when present while preserving older-host local fallback', () => {
+    const body: AgentJournalTurnItem = {
+      kind: 'turn',
+      turnId: 't1',
+      state: 'completed',
+      startedAt: 1_000,
+      userItemId: 'turn-1',
+      durationMs: 7_172
+    }
+    const statuses = selectNativeChatTurnStatuses(
+      {
+        'turn-1': { startedAt: 1_000, workedSeconds: 59 },
+        old: { startedAt: 1_000, workedSeconds: 9 }
+      },
+      {
+        activeTurnKey: 'turn-1',
+        isWorking: false,
+        hasCurrentTurnResponse: true,
+        settledByTurn: selectStructuredAgentSettledTurns([
+          userItem('turn-1'),
+          userItem('old'),
+          { itemId: 'lifecycle-1', revision: 1, sequence: 1, observedAt: 10_000, body },
+          {
+            itemId: 'lifecycle-old',
+            revision: 1,
+            sequence: 2,
+            observedAt: 10_000,
+            body: { kind: 'turn', turnId: 'old', userItemId: 'old', state: 'completed' }
+          }
+        ])
+      }
+    )
+    expect(statuses.active?.workedSeconds).toBe(7)
+    expect(statuses.completedByTurn.old?.workedSeconds).toBe(9)
+  })
+})

kbeautyarena/orca

src/main/native-chat/agent-session-journal/journal-row-schema-version.test.ts
@@ -1,0 +1,66 @@
+import { mkdtemp, rm } from 'node:fs/promises'
+import { tmpdir } from 'node:os'
+import { join } from 'node:path'
+import { afterEach, beforeEach, describe, expect, it } from 'vitest'
+import { agentJournalTurnBody } from '../../../shared/agent-session-turn-record'
+import { openJournalDatabase } from './journal-database'
+import { journalDatabaseFile } from './journal-paths'
+import { createTrackedJournalOpener } from './journal-store-test-open'
+
+// Which rows an older host can still read: only rows that carry a turn item
+// are stamped with the version it does not know, and the epoch row never is.
+// Read raw: the reader upcasts every row to the current version, so only the
+// stored row_json says what an older build would see.
+describe('journal row schema versions', () => {
+  let root = ''
+  const opener = createTrackedJournalOpener()
+
+  beforeEach(async () => {
+    root = await mkdtemp(join(tmpdir(), 'orca-journal-row-version-'))
+  })
+  afterEach(async () => {
+    await opener.closeAll()
+    await rm(root, { recursive: true, force: true })
+  })
+
+  it('stamps v3 only on rows that carry a turn item', async () => {
+    const journal = await opener.open({
+      identity: {
+        sessionId: 'session-1',
+        workspaceId: 'workspace-1',
+        hostId: 'local',
+        agent: 'codex',
+        providerHandle: { kind: 'codex', threadId: 'thread-1' }
+      },
+      now: () => 1_000,
+      journalDir: join(root, 'session-1')
+    })
+    const identity = { provider: 'orca' as const, clientMessageId: 'm1' }
+    await journal.appendItem(
+      identity,
+      { kind: 'message', role: 'user', blocks: [{ type: 'text', text: 'hi' }] },
+      { fence: 1 }
+    )
+    await journal.appendItem(
+      { provider: 'legacy', agent: 'codex', sessionId: 'session-1', recordId: 'turn-lifecycle:t1' },
+      agentJournalTurnBody({ turnId: 't1', state: 'running', startedAt: 1_000 }),
+      { fence: 1 }
+    )
+    await journal.close()
+    const opened = openJournalDatabase(journalDatabaseFile(join(root, 'session-1')))
+    try {
+      const stored = opened.db
+        .prepare('SELECT row_json FROM journal_rows ORDER BY seq')
+        .all()
+        .map((row) => JSON.parse(String((row as { row_json: string }).row_json)))
+        .map((row: { kind: string; v: number }) => [row.kind, row.v])
+      expect(stored).toEqual([
+        ['epoch', 2],
+        ['item', 2],
+        ['item', 3]
+      ])
+    } finally {
+      opened.db.close()
+    }
+  })
+})

src/main/native-chat/agent-session-wire/structured-agent-session-subscribers.test.ts
@@ -68,8 +68,54 @@ describe('AgentSessionSubscribers', () => {
           submissions: []
         },
         fence: 7,
+        hostNow: expect.any(Number),
         activity: null
       }
+    ])
+  })
+
+  it('stamps the host clock once per published frame', async () => {
+    const journal = await journals.open({
+      identity: {
+        sessionId: SESSION,
+        workspaceId: 'workspace-1',
+        hostId: 'local',
+        agent: 'codex',
+        providerHandle: { kind: 'codex', threadId: 'thread-1' }
+      },
+      journalDir: join(root, 'clock-journal')
+    })
+    let now = 1_000
+    const events: AgentSessionSubscribeEvent[] = []
+    const subscribers = new AgentSessionSubscribers({ now: () => (now += 1) })
+    const emit = (event: AgentSessionSubscribeEvent): void => {
+      events.push(event)
+    }
+    subscribers.open({ id: 'one', sessionId: SESSION, journal, fence: 1, emit })
+    subscribers.open({ id: 'two', sessionId: SESSION, journal, fence: 1, emit })
+    await journal.appendItem(
+      { provider: 'orca', clientMessageId: 'clocked' },
+      { kind: 'status', text: 'Clocked' },
+      { fence: 1 }
+    )
+    subscribers.publish(SESSION, journal)
+    subscribers.handoff(SESSION, 1, { owner: 'native' } as AgentSessionHandoffStatus)
+    subscribers.reset(SESSION, journal, 'epoch_changed', 1)
+
+    expect(events.map((event) => ('hostNow' in event ? event.hostNow : null))).toEqual([
+      1_001, 1_002,
+      // Both subscribers of one publication read the same clock sample.
+      1_003, 1_003, 1_004, 1_004, 1_005, 1_005
+    ])
+    expect(events.map((event) => event.type)).toEqual([
+      'snapshot',
+      'snapshot',
+      'batch',
+      'batch',
+      'batch',
+      'batch',
+      'reset',
+      'reset'
     ])
   })
 
@@ -101,6 +147,7 @@ describe('AgentSessionSubscribers', () => {
       type: 'batch',
       sessionId: SESSION,
       fence: 7,
+      hostNow: expect.any(Number),
       commands,
       batch: { cursor: journal.cursor(), items: [], removedItemIds: [], submissions: [] }
     })
@@ -245,6 +292,7 @@ describe('AgentSessionSubscribers', () => {
         submissions: []
       },
       fence: 2,
+      hostNow: expect.any(Number),
       handoff
     })
   })
@@ -284,6 +332,7 @@ describe('AgentSessionSubscribers', () => {
       sessionId: SESSION,
       batch: { cursor, items: [], removedItemIds: [], submissions: [] },
       fence: 2,
+      hostNow: expect.any(Number),
       backgroundTasks
     })
 
@@ -330,6 +379,7 @@ describe('AgentSessionSubscribers', () => {
       sessionId: SESSION,
       batch: { cursor, items: [], removedItemIds: [], submissions: [] },
       fence: 1,
+      hostNow: expect.any(Number),
       activity: { turnId: 'turn-1', text: 'Inspecting the session wire' }
     })
 

src/shared/native-chat-unverifiable-turn-status.test.ts
@@ -1,0 +1,134 @@
+import { describe, expect, it } from 'vitest'
+import type { AgentJournalRenderItem, AgentJournalTurnItem } from './agent-session-journal-types'
+import { reduceNativeChatTurnTiming, selectNativeChatTurnStatuses } from './native-chat-turn-status'
+import { selectStructuredAgentSettledTurns } from './structured-agent-session-turn-timing'
+
+function userItem(itemId: string): AgentJournalRenderItem {
+  return {
+    itemId,
+    revision: 0,
+    sequence: 0,
+    observedAt: 1_000,
+    body: { kind: 'message', role: 'user', blocks: [{ type: 'text', text: 'hello' }] }
+  }
+}
+
+describe('authoritative unknown turn duration at the shared status consumer', () => {
+  it.each([1_000, undefined])(
+    'does not convert a running turn to local Worked for after unverifiable recovery (start %s)',
+    (startedAt) => {
+      const user: AgentJournalRenderItem = {
+        itemId: 'orca:u1',
+        revision: 0,
+        sequence: 1,
+        observedAt: 1_000,
+        body: { kind: 'message', role: 'user', blocks: [{ type: 'text', text: 'hello' }] }
+      }
+      const recoveredTurn: AgentJournalRenderItem = {
+        itemId: 'legacy:codex:s:turn-lifecycle%3At1',
+        revision: 2,
+        sequence: 2,
+        observedAt: 60_000,
+        body: {
+          kind: 'turn',
+          turnId: 't1',
+          state: 'unverifiable',
+          ...(startedAt === undefined ? {} : { startedAt, userItemId: user.itemId })
+        }
+      }
+      const keys = new Set([user.itemId])
+      const running = reduceNativeChatTurnTiming(
+        {},
+        {
+          activeTurnKey: user.itemId,
+          validTurnKeys: keys,
+          isWorking: true,
+          workingStartedAt: 1_000,
+          now: 1_000
+        }
+      )
+      const locallyStopped = reduceNativeChatTurnTiming(running, {
+        activeTurnKey: user.itemId,
+        validTurnKeys: keys,
+        isWorking: false,
+        workingStartedAt: null,
+        now: 60_000
+      })
+      const options = {
+        activeTurnKey: user.itemId,
+        isWorking: false,
+        hasCurrentTurnResponse: true,
+        settledByTurn: selectStructuredAgentSettledTurns([user, recoveredTurn])
+      }
+
+      const mounted = selectNativeChatTurnStatuses(locallyStopped, options)
+      const reloaded = selectNativeChatTurnStatuses({}, options)
+      expect(mounted).toEqual(reloaded)
+      expect(mounted.active).toBeNull()
+      expect(mounted.completedByTurn).toEqual({})
+    }
+  )
+
+  it.each(['running', 'completed', 'interrupted'] as const)(
+    'suppresses local completion for a host-recorded %s turn without an endpoint',
+    (state) => {
+      const item: AgentJournalRenderItem = {
+        itemId: 'lifecycle-1',
+        revision: 1,
+        sequence: 1,
+        observedAt: 1_000,
+        body: { kind: 'turn', turnId: 't1', state, startedAt: 1_000, userItemId: 'turn-1' }
+      }
+      const statuses = selectNativeChatTurnStatuses(
+        {
+          'turn-1': { startedAt: 1_000, workedSeconds: 59 },
+          next: { startedAt: 60_000, workedSeconds: null }
+        },
+        {
+          activeTurnKey: 'next',
+          isWorking: true,
+          hasCurrentTurnResponse: true,
+          settledByTurn: selectStructuredAgentSettledTurns([userItem('turn-1'), item])
+        }
+      )
+      expect(statuses.completedByTurn).toEqual({})
+      expect(statuses.active).toEqual({ startedAt: 60_000, workedSeconds: null, thinking: false })
+    }
+  )
+
+  it('uses provider duration when present while preserving older-host local fallback', () => {
+    const body: AgentJournalTurnItem = {
+      kind: 'turn',
+      turnId: 't1',
+      state: 'completed',
+      startedAt: 1_000,
+      userItemId: 'turn-1',
+      durationMs: 7_172
+    }
+    const statuses = selectNativeChatTurnStatuses(
+      {
+        'turn-1': { startedAt: 1_000, workedSeconds: 59 },
+        old: { startedAt: 1_000, workedSeconds: 9 }
+      },
+      {
+        activeTurnKey: 'turn-1',
+        isWorking: false,
+        hasCurrentTurnResponse: true,
+        settledByTurn: selectStructuredAgentSettledTurns([
+          userItem('turn-1'),
+          userItem('old'),
+          { itemId: 'lifecycle-1', revision: 1, sequence: 1, observedAt: 10_000, body },
+          {
+            itemId: 'lifecycle-old',
+            revision: 1,
+            sequence: 2,
+            observedAt: 10_000,
+            body: { kind: 'turn', turnId: 'old', userItemId: 'old', state: 'completed' }
+          }
+        ])
+      }
+    )
+    expect(statuses.active?.workedSeconds).toBe(7)
+    expect(statuses.completedByTurn.old?.workedSeconds).toBe(9)
+  })
+})