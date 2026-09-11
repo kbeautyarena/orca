paidaxingyo666/Manta

refactor(agent-status): publish structured sessions into the hook server store (#19683)
* refactor(agent-status): publish structured sessions into the hook server store

Structured (native chat) sessions have no PTY and no hook script, so their
status never reached the hook server's store; #19217 gave `worktree ps` its
own adapter over the structured feed instead. The feed now writes every
projection into that store through a status sink the runtime wires, drops
the row when the host closes the session, and `worktree ps` reads the one
snapshot like every other agent.

Rows carry a `structuredHost` marker and the journal clock; they are never
persisted to last-status.json, and the main process does not forward them
to the renderer yet, whose feed bridge still owns them until it is retired.

Design and the two follow-ups: docs/reference/agent-status-store.md.

* chore: drop stray @pnpm/exe lockfile entry

An unrelated local pnpm run added @pnpm/exe as a packageManagerDependency
with no package.json change, so CI's --frozen-lockfile install failed
before any job ran.

* docs(agent-status): describe the step that actually landed

The design record claimed PR 1 deletes RuntimeAgentRowStore, drops the
retained-versus-hook reconciliation, stamps terminalHandle on OSC rows, and
tags rows with a source field of 'structured-host'. None of that is true of
the shipped code: the retained store and its reconciliation are still in
place, and the row field is structuredHost: 'held' | 'owned'.

AGENTS.md points every future contributor here before they touch agent
status, so split the roadmap into the 1a that landed and the 1b that has not,
and name the fields the code actually writes.

* fix(agent-status): pair session removal with the status-row forget

A session dropped from the host's map without an explicit forget left its row
in the store forever: `structuredHostOwned` bypasses the staleness check, so a
failed re-attach (the Claude rewind path reaches one) stranded a permanent

src/main/runtime/manta-runtime-tests/worktree-ps-structured-host.spec.ts
@@ -1,148 +1,75 @@
-import { afterEach, describe, expect, it, vi } from 'vitest'
+import { beforeEach, describe, expect, it, vi } from 'vitest'
 import { MantaRuntimeService } from '../manta-runtime-test-mocks.spec'
 import { TEST_WORKTREE_ID, store } from '../manta-runtime-test-fixtures.spec'
-import type { AgentJournalRenderItem } from '../../../shared/agent-session-journal-types'
-import type {
-  AgentSessionStatusEvent,
-  AgentSessionStatusSummary
-} from '../../../shared/agent-session-wire'
-import type { AgentSessionJournal } from '../../native-chat/agent-session-journal/journal-store'
-import type { StructuredAgentSessionHost } from '../../native-chat/agent-session-wire/structured-agent-session-host'
-import {
-  getStructuredAgentSessionHost,
-  setStructuredAgentSessionHost
-} from '../../native-chat/agent-session-wire/structured-agent-session-registry'
-import { StructuredAgentSessionStatusFeed } from '../../native-chat/agent-session-wire/structured-agent-session-status-feed'
+import { AgentHookServer, _internals } from '../../agent-hooks/server'
+
+vi.mock('../../telemetry/client', () => ({ track: vi.fn() }))
+vi.mock('../../telemetry/cohort-classifier', () => ({
+  getCohortAtEmit: vi.fn(() => ({ nth_repo_added: 2 }))
+}))
 
 /**
- * The production wiring, not the projection. Both structured-row suites call
- * `attachRuntimeWorktreeAgentRows` directly with summaries they built themselves, so nothing
- * executed `getWorktreePs`'s own `getStructuredAgentSessionHost()?.liveSessionStatusSummaries()`
- * — and that file carries `@ts-nocheck`, so renaming the accessor stayed green in typecheck AND
- * in the suite while `manta worktree ps` and mobile's poll would throw for every user.
+ * The production read, not the projection. The structured-row suites feed
+ * `attachRuntimeWorktreeAgentRows` a snapshot they built themselves; this executes `getWorktreePs`
+ * in `manta-runtime-get-worktree-ps.ts`, which is `@ts-nocheck`, so a renamed dependency there stays
+ * green in typecheck while `manta worktree ps` and mobile's poll would list nothing.
  */
+const SESSION = 'a1b2c3d4-e5f6-4a7b-8c9d-0e1f2a3b4c5d'
 
-const HELD_SESSION = 'a1b2c3d4-e5f6-4a7b-8c9d-0e1f2a3b4c5d'
-const FORGOTTEN_SESSION = 'b2c3d4e5-f6a7-4b8c-9d0e-1f2a3b4c5d6e'
-const OBSERVED_AT = 1_757_030_400_000
+beforeEach(() => {
+  _internals.resetCachesForTests()
+})
 
-function runningTurn(prompt: string): AgentJournalRenderItem[] {
-  return [
-    {
-      itemId: 'user-1',
-      sequence: 1,
-      revision: 1,
-      observedAt: OBSERVED_AT,
-      body: { kind: 'message', role: 'user', blocks: [{ type: 'text', text: prompt }] }
-    },
-    {
-      itemId: 'turn-1',
-      sequence: 2,
-      revision: 1,
-      observedAt: OBSERVED_AT,
-      body: {
-        kind: 'status',
-        text: 'Working',
-        turnLifecycle: { turnId: 'turn-1', state: 'running' }
-      }
-    }
-  ]
-}
+describe('worktree ps reads structured sessions from the agent-status store', () => {
+  it('lists a host-held structured session with no terminal behind it', async () => {
+    const statusStore = new AgentHookServer()
+    statusStore.ingestStructuredStatus({
+      sessionId: SESSION,
+      workspaceId: TEST_WORKTREE_ID,
+      agent: 'claude',
+      status: 'working',
+      hostExecutionOwned: true,
+      latestPrompt: 'ship the thing',
+      updatedAt: 1_757_030_400_000
+    })
+    const getAgentStatusSnapshot = vi.fn(() => statusStore.getStatusSnapshot())
 
-function journalWith(prompt: string): AgentSessionJournal {
-  return {
-    isReadOnly: false,
-    lastActivityAt: () => OBSERVED_AT,
-    // This journal never changes, so a real one would hold its cursor steady.
-    cursor: () => ({ epoch: 1, sequence: 1 }),
-    snapshot: () => ({ items: runningTurn(prompt) })
-  } as unknown as AgentSessionJournal
-}
-
-/** A real feed the host still holds one session on, having forgotten the other. Its `published`
- *  cache never retracts, so the two views genuinely differ. */
-function statusFeed(): StructuredAgentSessionStatusFeed {
-  const session = (prompt: string) => ({
-    journal: journalWith(prompt),
-    params: { location: { workspaceId: TEST_WORKTREE_ID }, provider: 'claude' as const }
-  })
-  const sessions = new Map([
-    [HELD_SESSION, session('ship the thing')],
-    [FORGOTTEN_SESSION, session('rm the branch')]
-  ])
-  const feed = new StructuredAgentSessionStatusFeed({
-    sessions,
-    getRecord: () => null,
-    now: () => OBSERVED_AT
-  })
-  feed.publish(HELD_SESSION)
-  feed.publish(FORGOTTEN_SESSION)
-  // `forget-session`, the last eviction step, drops the session and leaves the feed alone.
-  sessions.delete(FORGOTTEN_SESSION)
-  return feed
-}
-
-/** Everything the feed retained, read through the snapshot a subscriber opens on. No production
- *  code reads this; it is here so swapping the call site back to a whole-cache read is a one-line
- *  edit that this suite must catch. */
-function retainedSummaries(feed: StructuredAgentSessionStatusFeed): AgentSessionStatusSummary[] {
-  let retained: AgentSessionStatusSummary[] = []
-  feed.subscribe({
-    id: 'retained-probe',
-    emit: (event: AgentSessionStatusEvent) => {
-      if (event.type === 'snapshot') {
-        retained = event.sessions
-      }
-    }
-  })()
-  return retained
-}
-
-function installHost(feed: StructuredAgentSessionStatusFeed) {
-  const liveSessionStatusSummaries = vi.fn(() => feed.liveSessionSummaries())
-  const retainedSessionStatusSummaries = vi.fn(() => retainedSummaries(feed))
-  // Typed against the real host, so renaming the accessor on the class reddens `tc` here — the
-  // caller cannot, because `manta-runtime-get-worktree-ps.ts` is `@ts-nocheck`.
-  const host: Pick<StructuredAgentSessionHost, 'liveSessionStatusSummaries'> & {
-    retainedSessionStatusSummaries: () => AgentSessionStatusSummary[]
-  } = { liveSessionStatusSummaries, retainedSessionStatusSummaries }
-  setStructuredAgentSessionHost(host as unknown as StructuredAgentSessionHost)
-  return { liveSessionStatusSummaries, retainedSessionStatusSummaries }
-}
-
-describe('worktree ps reads the installed structured host', () => {
-  afterEach(() => {
-    setStructuredAgentSessionHost(null)
-  })
-
-  it('reports the held session and asks the host for its live summaries', async () => {
-    const feed = statusFeed()
-    const { liveSessionStatusSummaries } = installHost(feed)
-
-    const { worktrees } = await new MantaRuntimeService(store).getWorktreePs()
+    const { worktrees } = await new MantaRuntimeService(store, undefined, {
+      getAgentStatusSnapshot
+    }).getWorktreePs()
 
     const worktree = worktrees.find((entry) => entry.worktreeId === TEST_WORKTREE_ID)
     expect(worktree).toBeDefined()
-    // Exactly one: the forgotten session is still in the feed's retained cache, so a call site
-    // that enumerated that cache instead would report two.
     expect(worktree?.agents).toHaveLength(1)
     expect(worktree?.agents[0]).toMatchObject({
       state: 'working',
       agentType: 'claude',
-      prompt: 'ship the thing'
+      prompt: 'ship the thing',
+      structuredHostOwned: true
     })
-    // Pins the call site to the live-intersecting accessor, not merely to some accessor.
-    expect(liveSessionStatusSummaries).toHaveBeenCalledTimes(1)
+    expect(worktree?.status).toBe('working')
+    expect(getAgentStatusSnapshot).toHaveBeenCalledTimes(1)
   })
 
-  it('succeeds with no structured rows when no host is installed', async () => {
-    // Guard the guard: these specs share one module registry, so state the premise.
-    expect(getStructuredAgentSessionHost()).toBeNull()
+  it('lists nothing once the host has dropped the session', async () => {
+    const statusStore = new AgentHookServer()
+    statusStore.ingestStructuredStatus({
+      sessionId: SESSION,
+      workspaceId: TEST_WORKTREE_ID,
+      agent: 'claude',
+      status: 'attention',
+      latestPrompt: 'rm the branch',
+      updatedAt: 1_757_030_400_000
+    })
+    statusStore.dropStructuredStatus(SESSION)
 
-    const { worktrees } = await new MantaRuntimeService(store).getWorktreePs()
+    const { worktrees } = await new MantaRuntimeService(store, undefined, {
+      getAgentStatusSnapshot: () => statusStore.getStatusSnapshot()
+    }).getWorktreePs()
 
     const worktree = worktrees.find((entry) => entry.worktreeId === TEST_WORKTREE_ID)
     expect(worktree).toBeDefined()
     expect(worktree?.agents).toEqual([])
+    expect(worktree?.status).not.toBe('permission')
   })
 })

src/main/runtime/rpc/methods/orchestration/worker/agent-status-producer-census.test.ts
@@ -95,6 +95,11 @@ const CENSUS: readonly CensusRow[] = [
     role: 'binds the hook server snapshot into the runtime deps'
   },
   {
+    path: 'main/mantad/mantad-entry.ts',
+    kind: 'wiring',
+    role: 'binds the same snapshot and structured sink into the headless mantad runtime deps'
+  },
+  {
     path: 'main/runtime/manta-runtime-state-fields.ts',
     kind: 'wiring',
     role: 'stores the snapshot deps on the runtime'

kbeautyarena/orca

src/main/runtime/orca-runtime-tests/worktree-ps-structured-host.spec.ts
@@ -1,148 +1,75 @@
-import { afterEach, describe, expect, it, vi } from 'vitest'
+import { beforeEach, describe, expect, it, vi } from 'vitest'
 import { OrcaRuntimeService } from '../orca-runtime-test-mocks.spec'
 import { TEST_WORKTREE_ID, store } from '../orca-runtime-test-fixtures.spec'
-import type { AgentJournalRenderItem } from '../../../shared/agent-session-journal-types'
-import type {
-  AgentSessionStatusEvent,
-  AgentSessionStatusSummary
-} from '../../../shared/agent-session-wire'
-import type { AgentSessionJournal } from '../../native-chat/agent-session-journal/journal-store'
-import type { StructuredAgentSessionHost } from '../../native-chat/agent-session-wire/structured-agent-session-host'
-import {
-  getStructuredAgentSessionHost,
-  setStructuredAgentSessionHost
-} from '../../native-chat/agent-session-wire/structured-agent-session-registry'
-import { StructuredAgentSessionStatusFeed } from '../../native-chat/agent-session-wire/structured-agent-session-status-feed'
+import { AgentHookServer, _internals } from '../../agent-hooks/server'
+
+vi.mock('../../telemetry/client', () => ({ track: vi.fn() }))
+vi.mock('../../telemetry/cohort-classifier', () => ({
+  getCohortAtEmit: vi.fn(() => ({ nth_repo_added: 2 }))
+}))
 
 /**
- * The production wiring, not the projection. Both structured-row suites call
- * `attachRuntimeWorktreeAgentRows` directly with summaries they built themselves, so nothing
- * executed `getWorktreePs`'s own `getStructuredAgentSessionHost()?.liveSessionStatusSummaries()`
- * — and that file carries `@ts-nocheck`, so renaming the accessor stayed green in typecheck AND
- * in the suite while `orca worktree ps` and mobile's poll would throw for every user.
+ * The production read, not the projection. The structured-row suites feed
+ * `attachRuntimeWorktreeAgentRows` a snapshot they built themselves; this executes `getWorktreePs`
+ * in `orca-runtime-get-worktree-ps.ts`, which is `@ts-nocheck`, so a renamed dependency there stays
+ * green in typecheck while `orca worktree ps` and mobile's poll would list nothing.
  */
+const SESSION = 'a1b2c3d4-e5f6-4a7b-8c9d-0e1f2a3b4c5d'
 
-const HELD_SESSION = 'a1b2c3d4-e5f6-4a7b-8c9d-0e1f2a3b4c5d'
-const FORGOTTEN_SESSION = 'b2c3d4e5-f6a7-4b8c-9d0e-1f2a3b4c5d6e'
-const OBSERVED_AT = 1_757_030_400_000
+beforeEach(() => {
+  _internals.resetCachesForTests()
+})
 
-function runningTurn(prompt: string): AgentJournalRenderItem[] {
-  return [
-    {
-      itemId: 'user-1',
-      sequence: 1,
-      revision: 1,
-      observedAt: OBSERVED_AT,
-      body: { kind: 'message', role: 'user', blocks: [{ type: 'text', text: prompt }] }
-    },
-    {
-      itemId: 'turn-1',
-      sequence: 2,
-      revision: 1,
-      observedAt: OBSERVED_AT,
-      body: {
-        kind: 'status',
-        text: 'Working',
-        turnLifecycle: { turnId: 'turn-1', state: 'running' }
-      }
-    }
-  ]
-}
+describe('worktree ps reads structured sessions from the agent-status store', () => {
+  it('lists a host-held structured session with no terminal behind it', async () => {
+    const statusStore = new AgentHookServer()
+    statusStore.ingestStructuredStatus({
+      sessionId: SESSION,
+      workspaceId: TEST_WORKTREE_ID,
+      agent: 'claude',
+      status: 'working',
+      hostExecutionOwned: true,
+      latestPrompt: 'ship the thing',
+      updatedAt: 1_757_030_400_000
+    })
+    const getAgentStatusSnapshot = vi.fn(() => statusStore.getStatusSnapshot())
 
-function journalWith(prompt: string): AgentSessionJournal {
-  return {
-    isReadOnly: false,
-    lastActivityAt: () => OBSERVED_AT,
-    // This journal never changes, so a real one would hold its cursor steady.
-    cursor: () => ({ epoch: 1, sequence: 1 }),
-    snapshot: () => ({ items: runningTurn(prompt) })
-  } as unknown as AgentSessionJournal
-}
-
-/** A real feed the host still holds one session on, having forgotten the other. Its `published`
- *  cache never retracts, so the two views genuinely differ. */
-function statusFeed(): StructuredAgentSessionStatusFeed {
-  const session = (prompt: string) => ({
-    journal: journalWith(prompt),
-    params: { location: { workspaceId: TEST_WORKTREE_ID }, provider: 'claude' as const }
-  })
-  const sessions = new Map([
-    [HELD_SESSION, session('ship the thing')],
-    [FORGOTTEN_SESSION, session('rm the branch')]
-  ])
-  const feed = new StructuredAgentSessionStatusFeed({
-    sessions,
-    getRecord: () => null,
-    now: () => OBSERVED_AT
-  })
-  feed.publish(HELD_SESSION)
-  feed.publish(FORGOTTEN_SESSION)
-  // `forget-session`, the last eviction step, drops the session and leaves the feed alone.
-  sessions.delete(FORGOTTEN_SESSION)
-  return feed
-}
-
-/** Everything the feed retained, read through the snapshot a subscriber opens on. No production
- *  code reads this; it is here so swapping the call site back to a whole-cache read is a one-line
- *  edit that this suite must catch. */
-function retainedSummaries(feed: StructuredAgentSessionStatusFeed): AgentSessionStatusSummary[] {
-  let retained: AgentSessionStatusSummary[] = []
-  feed.subscribe({
-    id: 'retained-probe',
-    emit: (event: AgentSessionStatusEvent) => {
-      if (event.type === 'snapshot') {
-        retained = event.sessions
-      }
-    }
-  })()
-  return retained
-}
-
-function installHost(feed: StructuredAgentSessionStatusFeed) {
-  const liveSessionStatusSummaries = vi.fn(() => feed.liveSessionSummaries())
-  const retainedSessionStatusSummaries = vi.fn(() => retainedSummaries(feed))
-  // Typed against the real host, so renaming the accessor on the class reddens `tc` here — the
-  // caller cannot, because `orca-runtime-get-worktree-ps.ts` is `@ts-nocheck`.
-  const host: Pick<StructuredAgentSessionHost, 'liveSessionStatusSummaries'> & {
-    retainedSessionStatusSummaries: () => AgentSessionStatusSummary[]
-  } = { liveSessionStatusSummaries, retainedSessionStatusSummaries }
-  setStructuredAgentSessionHost(host as unknown as StructuredAgentSessionHost)
-  return { liveSessionStatusSummaries, retainedSessionStatusSummaries }
-}
-
-describe('worktree ps reads the installed structured host', () => {
-  afterEach(() => {
-    setStructuredAgentSessionHost(null)
-  })
-
-  it('reports the held session and asks the host for its live summaries', async () => {
-    const feed = statusFeed()
-    const { liveSessionStatusSummaries } = installHost(feed)
-
-    const { worktrees } = await new OrcaRuntimeService(store).getWorktreePs()
+    const { worktrees } = await new OrcaRuntimeService(store, undefined, {
+      getAgentStatusSnapshot
+    }).getWorktreePs()
 
     const worktree = worktrees.find((entry) => entry.worktreeId === TEST_WORKTREE_ID)
     expect(worktree).toBeDefined()
-    // Exactly one: the forgotten session is still in the feed's retained cache, so a call site
-    // that enumerated that cache instead would report two.
     expect(worktree?.agents).toHaveLength(1)
     expect(worktree?.agents[0]).toMatchObject({
       state: 'working',
       agentType: 'claude',
-      prompt: 'ship the thing'
+      prompt: 'ship the thing',
+      structuredHostOwned: true
     })
-    // Pins the call site to the live-intersecting accessor, not merely to some accessor.
-    expect(liveSessionStatusSummaries).toHaveBeenCalledTimes(1)
+    expect(worktree?.status).toBe('working')
+    expect(getAgentStatusSnapshot).toHaveBeenCalledTimes(1)
   })
 
-  it('succeeds with no structured rows when no host is installed', async () => {
-    // Guard the guard: these specs share one module registry, so state the premise.
-    expect(getStructuredAgentSessionHost()).toBeNull()
+  it('lists nothing once the host has dropped the session', async () => {
+    const statusStore = new AgentHookServer()
+    statusStore.ingestStructuredStatus({
+      sessionId: SESSION,
+      workspaceId: TEST_WORKTREE_ID,
+      agent: 'claude',
+      status: 'attention',
+      latestPrompt: 'rm the branch',
+      updatedAt: 1_757_030_400_000
+    })
+    statusStore.dropStructuredStatus(SESSION)
 
-    const { worktrees } = await new OrcaRuntimeService(store).getWorktreePs()
+    const { worktrees } = await new OrcaRuntimeService(store, undefined, {
+      getAgentStatusSnapshot: () => statusStore.getStatusSnapshot()
+    }).getWorktreePs()
 
     const worktree = worktrees.find((entry) => entry.worktreeId === TEST_WORKTREE_ID)
     expect(worktree).toBeDefined()
     expect(worktree?.agents).toEqual([])
+    expect(worktree?.status).not.toBe('permission')
   })
 })

src/main/runtime/rpc/methods/orchestration/worker/agent-status-producer-census.test.ts
@@ -95,6 +95,11 @@ const CENSUS: readonly CensusRow[] = [
     role: 'binds the hook server snapshot into the runtime deps'
   },
   {
+    path: 'main/orcad/orcad-entry.ts',
+    kind: 'wiring',
+    role: 'binds the same snapshot and structured sink into the headless orcad runtime deps'
+  },
+  {
     path: 'main/runtime/orca-runtime-state-fields.ts',
     kind: 'wiring',
     role: 'stores the snapshot deps on the runtime'